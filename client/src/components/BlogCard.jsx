119fed8faa5989e493f9e9b18ff2fc58f4e476bf
ved142 <ved78142@gmail.com>
2023-04-14 13:35:35 +0530

    blog-view screen

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import "../../css/blogcard.css";
+import { useNavigate } from "react-router-dom";
 
-const BlogCard = () => {
+const BlogCard = (props) => {
+  const navigate = useNavigate();
+  console.log(props.blog_data);
   return (
-    <Box className="blog_card">
+    <Box
+      onClick={() => {
+        console.log(props.blog_data);
+        navigate("/blog-viewer", {
+          state: props.blog_data,
+        });
+      }}
+      className="blog_card"
+    >
       <div className="image_card">
         <img
           className="card_img"
-          src="https://plus.unsplash.com/premium_photo-1663013123196-f83decee810f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+          src={props.blog_data.imgUrl1}
           alt="this is image"
         />
       </div>
       <div className="card_data">
         <p id="card_date">08.08.2023</p>
-        <p id="card_title">May this title help you doing your work</p>
-        <p className="card_para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        </p>
+        <p id="card_title">{props.blog_data.title}</p>
+        <p className="card_para">{props.blog_data.para1.substr(1, 80)}</p>
       </div>
     </Box>
   );
---
+++ client/src/components/View_Blog.jsx
@@ -0,0 +1,36 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import { useLocation } from "react-router-dom";
+import "../../css/viewblog.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const View_Blog = () => {
+  const { state } = useLocation();
+  console.log(state);
+  const title = state.title;
+  const tag = state.tag;
+  const para1 = state.para1;
+  const para2 = state.para2;
+  const image1Url = state.imgUrl1;
+  const image2Url = state.imgUrl2;
+  console.log(image1Url);
+  return (
+    <>
+      <Navbar />
+      <div className="blog_div">
+        <div className="head_blog">
+          <p style={{ color: "#00ACED" }}>{tag}</p>
+          <h1 id="blgvw_title">{title}</h1>
+          <img className="blog_view_img1" alt="pic_1" src={image1Url} />
+        </div>
+        <p id="blgvw_p1">{para1}</p>
+        <img className="blog_view_img1" alt="pic_2" src={image2Url} />
+        <p id="blgvw_p1">{para2}</p>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default View_Blog;
